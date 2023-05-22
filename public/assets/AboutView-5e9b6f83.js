6299250f5aaa36b810140c102c08eb9b99bef239
Daniel Werner <vernerd@gmail.com>
2023-05-22 10:19:25 +0200

    Build app

---
+++ public/assets/AboutView-5e9b6f83.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o as t,c as o,a as s}from"./index-0c988fe3.js";const _={},c={class:"about"},a=s("h1",null,"This is an about page",-1),n=[a];function r(i,u){return t(),o("div",c,n)}const f=e(_,[["render",r]]);export{f as default};
