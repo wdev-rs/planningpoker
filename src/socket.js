a4bc61db9fbcc8f2a8fc52bc6dafc698f1f4686a
Daniel Werner <vernerd@gmail.com>
2023-05-23 13:13:00 +0200

    Build app

--- src/socket.js
+++ src/socket.js
@@ -12,9 +12,9 @@ export const state = reactive({
 });
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+// const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
 
-export const socket = io(URL, {autoConnect: false});
+export const socket = io('/', {autoConnect: false});
 
 socket.on("connect", () => {
     state.connected = true;
